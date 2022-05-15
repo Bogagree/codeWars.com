c710f6338b0ae83bd85341e33cfa43345fe53b78
Bogagree <bogatireshka@gmail.com>
2022-05-15 23:21:21 +0300

    feat: 7 kyu Chain me

--- app.js
+++ app.js
@@ -41,5 +41,24 @@ function makeNegative(num) {
 
     // return -Math.abs(num);
 
+}
 
-  }
+
+
+
+// 7 kyu Chain me
+
+//     Write a generic function chainer
+// Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+
+// The input for each function is the output of the previous function (except the first function, which takes the starting value as its input). Return the final value after execution is complete.
+
+
+function chain(input, fs) {
+    // implement the "chain" function
+      let a = input
+      for (let i=0; i<fs.length ; i++) {
+        a = fs[i](a)
+      }
+    return a
+      }
